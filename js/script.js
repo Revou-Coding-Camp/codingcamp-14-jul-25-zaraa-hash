4be6a29acd63a8b50708cda6f6b1557e89d2d57b
zaraa-hash <azzahrakamila93@gmail.com>
2025-07-19 10:58:39 +0700

    changes of mini project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// Ini Javascript External
+console.log("JavaScript is working!");
+function validateform() {
+    const nameInput = document.getElementById('name-input');
+    console.log(nameInput.value);
+
+    if (nameInput.value === '');
+    alert ('Please enter your name.');
+
+}
+    
